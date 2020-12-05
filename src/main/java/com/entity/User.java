763352937fae60c2976a0db85d2f97194054015b
WuLiangHang <632352954@qq.com>
2020-12-05 10:39:41 +0800

    我的提交

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -2,6 +2,15 @@ package com.entity;
 
 public class User {
     private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
